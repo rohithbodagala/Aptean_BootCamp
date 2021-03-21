7306ac11e37202daa9a941df8151d0b030190c4a
Rohith Bodagala <rohithbodagala.rit@gmail.com>
2021-03-21 23:36:28 +0530

    Added Assignment

---
+++ Assignment_March 19-22/3/FileBasic/FileBasic/CollectionMgr.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+namespace FileBasic
+{
+    class CollectionMgr
+    {
+        string FileName = "";
+        List<string> FilesCollection;
+        public CollectionMgr()
+        {
+            string name = "";
+            FilesCollection = new List<string>();
+            string[] files = Directory.GetFiles(@"..\..\..\Files");
+            foreach(var file in files)
+            {
+                name = file.Substring(15);
+                FilesCollection.Add(name);
+            }
+
+        }
+        public string getFilePath(string fname)
+        {
+            return ConfigurationManager.AppSettings["root"].ToString() + fname;
+        }
+        public void getFilesFromDirectory()
+        {
+            foreach(var file in FilesCollection)
+            {
+                Console.WriteLine(file);
+            }
+        }
+        public void AddFile(string fname)
+        {
+            FileName = "";
+            
+            try
+            {
+                int charPos = fname.IndexOf(".");
+                string ext = fname.Substring(charPos + 1);
+                if (!ext.Equals("txt"))
+                {
+                    throw new Exception();
+                }
+                FileName += getFilePath(fname);
+                StreamWriter sw = new StreamWriter(FileName);
+                FilesCollection.Add(fname);
+                Console.WriteLine("Enter the Text that you want to write to the File:");
+                string str = Console.ReadLine();
+                sw.WriteLine(str);
+                sw.Flush();
+                sw.Close();
+                    
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: Invalid File Extension!");
+            }
+
+
+
+        }
+
+        public void RemoveFile(string fname)
+        {
+            FileName = "";
+            FileName += getFilePath(fname);
+            if (isValidFile(FileName))
+            {
+                File.Delete(FileName);
+                FilesCollection.Remove(fname);
+            }
+
+        }
+        public int getCount(string FileName)
+        {
+            int counter = 0;
+            StreamReader sr = new StreamReader(FileName);
+
+            string delim = " ,.";
+            string[] fields = null;
+            string line = null;
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                line.Trim();
+                fields = line.Split(delim.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                counter += fields.Length;
+            }
+            sr.Close();
+            return counter;
+        }
+
+
+        public void GetTotalWordsInAFile(string fname)
+        {
+
+            FileName = getFilePath(fname);
+            if (isValidFile(FileName))
+            {
+                Console.WriteLine($"The word count is {getCount(FileName)}");
+                
+                
+            }
+
+
+        }
+
+        public void GetTotalWordsInAllFiles()
+        {
+            int TotalWord = 0;
+            foreach (string file in FilesCollection)
+            {
+                FileName = getFilePath(file);
+                TotalWord += getCount(FileName);
+            }
+            Console.WriteLine(TotalWord);
+
+        }
+        public bool isValidFile(string FileName)
+        {
+            try
+            {
+                if (!File.Exists(FileName))
+                {
+                    throw new FileNotFoundException();
+                }
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Error: File Does not Exist!");
+                return false;
+            }
+            
+
+        }
+    }
+
+}
---
+++ Assignment_March 19-22/3/FileBasic/FileBasic/FileExpectations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace FileBasic
+{
+
+    class FileExpectations
+    {
+        List<string> files = new List<string>();
+        string FileName = "";
+        
+
+        
+        static void Main(string[] args)
+        {
+            CollectionMgr obj = new CollectionMgr();
+            char choice = new char();
+            do
+            {
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("\t1.Add new file to collection");
+                Console.WriteLine("\t2.Delete a given file from collection");
+                Console.WriteLine("\t3.Count of words in a file");
+                Console.WriteLine("\t4.Count of words in all files");
+
+
+                int select = int.Parse(Console.ReadLine());
+                switch (select)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the file name to create:");
+                        string fname = Console.ReadLine();
+                        obj.AddFile(fname);
+                            break;
+                    case 2:
+                        obj.getFilesFromDirectory();
+                        Console.WriteLine("enter the file name to delete");
+                        fname = Console.ReadLine();
+                        obj.RemoveFile(fname);
+                            break;
+                    case 3:
+                        obj.getFilesFromDirectory();
+                        Console.WriteLine("Please enter the file name");
+                            fname = Console.ReadLine();
+                        obj.GetTotalWordsInAFile(fname);
+                        break;
+                    case 4:
+                            Console.WriteLine("Total Word in all the files are ");
+                            obj.GetTotalWordsInAllFiles();
+                            break;
+                    default: break;
+                }
+                Console.WriteLine(" Do you want to exit?  (Y/N)");
+                choice = char.Parse(Console.ReadLine());
+            } while (choice != 'Y');
+        }
+    }
+
+
+}
+
+
